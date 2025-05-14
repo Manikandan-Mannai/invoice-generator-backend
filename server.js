6474ff1cbff98849eab5e502e7fa192435c69c35
Manikandan-Mannai <iamanikandan.mannai@gmail.com>
2025-05-14 18:23:38 +0530

    OAuth + Passportjs completed

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
 import express from "express";
 import dotenv from "dotenv";
+import session from "express-session";
+import cors from "cors";
+import passport from "passport";
 
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import "./config/passport.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// MongoDB connection
 connectDB();
 
+// Middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
 app.use("/api/users", userRoutes);
+app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is running!");
